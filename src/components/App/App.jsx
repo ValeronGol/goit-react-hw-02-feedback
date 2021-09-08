93c0eed8fba3e4bdef540d1f0efd0225be61e30f
valeraron <77055501+valeraron@users.noreply.github.com>
2021-09-08 11:56:54 +0300

    refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -35,7 +35,7 @@ export default class App extends Component {
       <Conteiner>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
